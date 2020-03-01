2dc804724254fd008cb96a76287ea4ea234ea15b
Kelsie <kelsie.braidwood@digirati.com>
2020-03-01 18:43:44 +0000

    WIP with IIIF Vue Project

---
+++ components/OpenSeaDragon.vue
@@ -0,0 +1,41 @@
+<template>
+  <client-only placeholder="...Loading">
+    <div id="openseadragon1" style="width: 800px; height: 600px;"></div>
+  </client-only>
+</template>
+<script src="/openseadragon/openseadragon.min.js"></script>
+<script>
+if (process.client) {
+  require('openseadragon')
+}
+import OpenSeadragon from 'use-open-seadragon/lib/types/open-seadragon'
+
+const viewer = {
+  ...OpenSeadragon,
+  preserveViewport: true,
+  visibilityRatio: 1,
+  minZoomLevel: 1,
+  defaultZoomLevel: 1,
+  sequenceMode: true,
+  tileSources: [
+    {
+      '@context': 'http://iiif.io/api/image/2/context.json',
+      '@id':
+        'https://libimages1.princeton.edu/loris/pudl0001%2F4609321%2Fs42%2F00000001.jp2',
+      height: 7200,
+      width: 5233,
+      profile: ['http://iiif.io/api/image/2/level2.json'],
+      protocol: 'http://iiif.io/api/image',
+      tiles: [
+        {
+          scaleFactors: [1, 2, 4, 8, 16, 32],
+          width: 1024
+        }
+      ]
+    }
+  ]
+}
+export default {
+  title: 'OpenSeaDragon'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <OpenSeaDragon />
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import OpenSeaDragon from '~/components/OpenSeaDragon.vue'
+
+export default {
+  components: {
+    OpenSeaDragon
+  }
+}
+</script>
